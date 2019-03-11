189c80bac805ce58d4be1f22309fd5bd5de3f64b
hoonest <hoonest01@gmail.com>
2019-03-12 00:14:44 +0900

    Init

---
+++ rnn_test.py
@@ -0,0 +1,73 @@
+'''
+This script shows how to predict stock prices using a basic RNN
+'''
+import tensorflow as tf
+import numpy as np
+import matplotlib
+import os
+import speech_data
+
+tf.set_random_seed(777)  # reproducibility
+
+
+batch=speech_data.wave_batch_generator(10000,target=speech_data.Target.digits)
+trainX,trainY=next(batch)
+
+number_classes=10 # Digits
+
+# train Parameters
+seq_length = 7
+data_dim = 5
+hidden_dim = 10
+output_dim = 1
+learning_rate = 0.01
+iterations = 500
+
+# input place holders
+X = tf.placeholder(tf.float32, [None, seq_length, data_dim])
+Y = tf.placeholder(tf.float32, [None, 1])
+
+# build a LSTM network
+cell = tf.contrib.rnn.BasicLSTMCell(
+    num_units=hidden_dim, state_is_tuple=True, activation=tf.tanh)
+outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+Y_pred = tf.contrib.layers.fully_connected(
+    outputs[:, -1], output_dim, activation_fn=None)  # We use the last cell's output
+
+# cost/loss
+loss = tf.reduce_sum(tf.square(Y_pred - Y))  # sum of the squares
+# optimizer
+optimizer = tf.train.AdamOptimizer(learning_rate)
+train = optimizer.minimize(loss)
+
+# RMSE
+targets = tf.placeholder(tf.float32, [None, 1])
+predictions = tf.placeholder(tf.float32, [None, 1])
+rmse = tf.sqrt(tf.reduce_mean(tf.square(targets - predictions)))
+
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    # Training step
+    for i in range(iterations):
+        _, step_loss = sess.run([train, loss], feed_dict={
+                                X: trainX, Y: trainY})
+        print("[step: {}] loss: {}".format(i, step_loss))
+'''
+    # Test step
+    test_predict = sess.run(Y_pred, feed_dict={X: testX})
+    rmse_val = sess.run(rmse, feed_dict={
+                    targets: testY, predictions: test_predict})
+    print("RMSE: {}".format(rmse_val))
+
+    # Plot predictions
+    plt.plot(testY)
+    plt.plot(test_predict)
+    plt.xlabel("Time Period")
+    plt.ylabel("Stock Price")
+    plt.show()
+'''
+
+
+
